02fd5a613679a3b1adb932fc59b096c609b33ed7
Rares Dobre <rares99@gmail.com>
2019-10-02 19:40:16 +0300

    Both tables have data, foreign key constraint work

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A reservation system for the sales division
 
-Tutorials:
+Tutorials used:
 
 - https://www.techiediaries.com/node-sqlite-crud/
 - https://dev.to/lampewebdev/getting-sqlite-running-with-await-async-and-expressjs-polkajs-2b0g
--- app.js
+++ app.js
@@ -8,10 +8,7 @@ const main = async () => {
   }
 
   try {
-    console.log("[CREATE] Creating resources table");
-    await db.run(
-      "CREATE TABLE IF NOT EXISTS resources(resource_id INTEGER PRIMARY KEY AUTOINCREMENT, resource_name TEXT UNIQUE NOT NULL)"
-    );
+    await createTables();
 
     await insertResource("First resouce name");
     await insertResource("Second resouce name");
@@ -19,18 +16,44 @@ const main = async () => {
 
     await printResources();
 
-    await db.get("PRAGMA foreign_keys = ON");
-
-    console.log("[CREATE] Creating reservations table");
-    await db.run(
-      "CREATE TABLE IF NOT EXISTS reservations(reservation_id INTEGER PRIMARY KEY AUTOINCREMENT, start_date DATE NOT NULL, end_date DATE NOT NULL, resource_id INTEGER, owner_email TEXT NOT NULL, comments TEXT NOT NULL, FOREIGN KEY (resource_id) REFERENCES resources (resource_id))"
+    await insertReservation(
+      new Date(),
+      new Date(),
+      "1",
+      "email1@gmail.com",
+      "A looooooong comment"
+    );
+    await insertReservation(
+      new Date(),
+      new Date(),
+      "2",
+      "email2@gmail.com",
+      "Short com"
+    );
+    await insertReservation(
+      new Date(),
+      new Date(),
+      "3",
+      "email3@gmail.com",
+      "booo"
     );
 
-    console.log("[INSERT]Inserting data into reservations table");
-    //await db.run("INSERT INTO reservations(");
+    printReservations();
   } catch (e) {
     console.error(e);
   }
+};
+
+const createTables = async () => {
+  console.log("[CREATE] Creating resources table");
+  await db.run(
+    "CREATE TABLE IF NOT EXISTS resources(resource_id INTEGER PRIMARY KEY AUTOINCREMENT, resource_name TEXT UNIQUE NOT NULL)"
+  );
+  console.log("[CREATE] Creating reservations table");
+  await db.run(
+    "CREATE TABLE IF NOT EXISTS reservations(reservation_id INTEGER PRIMARY KEY AUTOINCREMENT, start_date DATE NOT NULL, end_date DATE NOT NULL, resource_id INTEGER, owner_email TEXT NOT NULL, comments TEXT NOT NULL, FOREIGN KEY (resource_id) REFERENCES resources (resource_id))"
+  );
+  await db.get("PRAGMA foreign_keys = ON");
 };
 
 const insertResource = async name => {
@@ -40,11 +63,45 @@ const insertResource = async name => {
   ]);
 };
 
+const insertReservation = async (
+  start_date,
+  end_date,
+  resource_id,
+  owner_email,
+  comments
+) => {
+  console.log("[INSERT] Inserting data into reservations table");
+  return await db.run(
+    "INSERT INTO reservations(start_date, end_date, resource_id, owner_email, comments) VALUES (?,?,?,?,?)",
+    [start_date, end_date, resource_id, owner_email, comments]
+  );
+};
+
 const printResources = async () => {
   console.log("[SELECT] Printing resources data");
   let rows = await db.all("SELECT * FROM resources");
   await rows.forEach(row =>
     console.log(row.resource_id + ": " + row.resource_name)
+  );
+};
+
+const printReservations = async () => {
+  console.log("[SELECT] Printing reservations data");
+  let rows = await db.all("SELECT * FROM reservations");
+  await rows.forEach(row =>
+    console.log(
+      row.reservation_id +
+        ": " +
+        new Date(row.start_date).toISOString() +
+        ", " +
+        new Date(row.end_date).toISOString() +
+        ", " +
+        row.resource_id +
+        ", " +
+        row.owner_email +
+        ", " +
+        row.comments
+    )
   );
 };
 
