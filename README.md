bef847701c4ddce65a13b29492164b847174e0fc
Rares Dobre <rares99@gmail.com>
2019-10-02 18:55:15 +0300

    Changed to async await

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Reservation-System
+
 A reservation system for the sales division
 
 Tutorials:
-	- https://www.techiediaries.com/node-sqlite-crud/
+
+- https://www.techiediaries.com/node-sqlite-crud/
+- https://dev.to/lampewebdev/getting-sqlite-running-with-await-async-and-expressjs-polkajs-2b0g
--- app.js
+++ app.js
@@ -1,37 +1,31 @@
-const sqlite3 = require("sqlite3");
+//const sqlite3 = require("sqlite3");
+const Database = require("sqlite-async");
 
-let db = new sqlite3.Database("./mydb.sqlite3", err => {
-  if (err) {
-    console.log("Error when creating the database", err);
-  } else {
-    console.log("Database created!");
-    /* Put code to create table(s) here */
-    createTable();
+const main = async () => {
+  try {
+    db = await Database.open(":memory:");
+  } catch (e) {
+    throw Error("can not access sqlite database");
   }
-});
 
-const createTable = () => {
-  console.log("create database table resources");
-  db.run(
-    "CREATE TABLE IF NOT EXISTS resources(resource_id INTEGER PRIMARY KEY AUTOINCREMENT, resource_name TEXT UNIQUE)",
-    insertData
-  );
+  try {
+    console.log("Creating resources table");
+    await db.run(
+      "CREATE TABLE IF NOT EXISTS resources(resource_id INTEGER PRIMARY KEY AUTOINCREMENT, resource_name TEXT UNIQUE)"
+    );
+    console.log("Inserting data into resources");
+    await db.run("INSERT INTO resources (resource_name) VALUES (?),(?),(?)", [
+      "First resource name",
+      "Second resource name",
+      "Third resouce name"
+    ]);
+    let rows = await db.all("SELECT * FROM resources");
+    rows.forEach(row =>
+      console.log(row.resource_id + ": " + row.resource_name)
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-const insertData = () => {
-  console.log("Insert data");
-  db.run(
-    "INSERT INTO resources (resource_name) VALUES (?)",
-    ["firstResourceName"],
-    readResources
-  );
-};
-
-const readResources = () => {
-  console.log("Read data from contacts");
-  db.all("SELECT * FROM resources", function(err, rows) {
-    rows.forEach(function(row) {
-      console.log(row.resource_id + ": " + row.resource_name);
-    });
-  });
-};
+main();
